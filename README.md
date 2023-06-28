62948a2cbdc998c730ad3ef4927a81a9c8f50b7a
Gabriel Tobias <gabrieltobias10@outlook.com>
2023-06-28 14:14:39 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pac - Man  Gaming Project
+Classic Pac-Man game made in Python. (unfinished project)
+<br>
+<br>
+
+![Imagem pacman](https://github.com/joaotinti75/Pacman_Curso_Pygame/blob/main/pacman-logo-1.png)
---
+++ constantes.py
@@ -0,0 +1,33 @@
+#Dimensões Da Tela
+
+LARGURA = 480
+ALTURA = 600
+
+#Nome Do Jogo
+
+TITULO_JOGO = "PAC-MAN"
+
+#FPS
+
+FPS = 30
+
+#Cores
+
+PRETO = (0, 0, 0) 
+AMARELO = (244, 233, 51)
+BRANCO = (255, 255, 255)
+
+#Imagens
+
+SPRITESHEET = "spritesheet.png"
+PACMAN_START_LOGO = "pacman-logo-1.png"
+
+#Audios
+
+MUSICA_START = 'intermission.wav'
+TECLA_START  = 'munch_1.wav'
+
+
+#Fonte
+
+FONTE = "arial"
